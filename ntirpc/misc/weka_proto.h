e6ddc88d3a2769a73b07fa4fd5c1d7171068c08b
Vinay Channagiri <vinay.channagiri@weka.io>
2023-09-27 22:22:58 -0700

    Populate per thread protocol specific information (WEKAPP-336437)

---
+++ ntirpc/misc/weka_proto.h
@@ -0,0 +1,93 @@
+#ifndef __WEKA_PROTO_H__
+#define __WEKA_PROTO_H__
+
+#ifndef KERNEL_MODULE
+#include <sys/stat.h>
+#endif
+
+#define WEKA_PROTO_MAX_KEYS  1
+#define WEKA_PROTO_MAX_DATA  32
+
+#define WEKA_PROTO_MAX_SLOTS 1024
+#define WEKA_PROTO_SLOT_SIZE 72
+#define WEKA_PROTO_SHMEM_SIZE 72 * 1024
+
+#define WEKA_PROTO_IOCTL  (('L' << 24) | ('O' << 16) | ('C' << 8) | 'P')
+
+#define WEKA_PROTO_DEV_NAME  "weka-proto"
+
+typedef enum {
+   WEKA_PROTO_KEY_NONE = 0,
+   WEKA_PROTO_KEY_ENDPOINTS = 1
+} weka_proto_key_t;
+
+typedef enum {
+    WEKA_PROTO_OP_NONE = 0,
+    WEKA_PROTO_OP_GETSLOT = 1,
+    WEKA_PROTO_OP_PUTSLOT = 2
+} weka_proto_op_t;
+
+typedef struct weka_proto_keymap_table {
+    weka_proto_key_t  keys[WEKA_PROTO_MAX_KEYS];
+} weka_proto_keymap_table_t;
+
+typedef struct weka_proto_slot {
+    int slot;
+} weka_proto_slot_t;
+
+typedef struct weka_proto_ioctl {
+    int   op;
+    union {
+        struct weka_proto_slot      getslot;
+        struct weka_proto_slot      putslot;
+    } u;
+} weka_proto_ioctl_t;
+
+#define WEKA_PROTO_INVALID_FIP_VERSION        0xFFFF
+#define WEKA_PROTO_MISMATCH_VERSION_ERR       125       /* ECANCELED */
+#define WEKA_PROTO_INVALID_OPCODE             0xFF
+#define WEKA_PROTO_INVALID_NAME               0
+
+#define WEKA_PROTO_NAME_NFS         1
+#define WEKA_PROTO_NAME_S3          2
+#define WEKA_PROTO_NAME_SMB         3
+#define WEKA_PROTO_NAME_MAX         4
+
+typedef struct weka_proto_client_info {
+    unsigned int ipaddr;
+    unsigned int xid;
+    unsigned char sub_xid;
+} weka_proto_client_info_t;
+
+typedef struct weka_proto_target_info {
+    unsigned int ipaddr;
+    unsigned short ver;
+} weka_proto_target_info_t;
+
+typedef struct weka_proto_endpoints_info {
+    unsigned char   name;
+    unsigned char   docache;
+    unsigned char   opcode;
+    weka_proto_client_info_t client;
+    weka_proto_target_info_t target;
+} weka_proto_endpoints_info_t;
+
+#ifdef KERNEL_MODULE
+extern int weka_proto_init(void);
+extern int weka_proto_shutdown(void);
+extern int weka_proto_get_endpoints_info(pid_t tid, weka_proto_endpoints_info_t *data);
+extern int weka_proto_set_endpoints_info(pid_t tid, weka_proto_endpoints_info_t *data);
+extern int weka_proto_clear_endpoints_info(pid_t tid);
+#else
+extern int weka_proto_init(void);
+extern int weka_proto_shutdown(void);
+extern int weka_proto_alloc_slot(void);
+extern int weka_proto_free_slot(int slot);
+extern int weka_proto_get_endpoints_info(int slot, weka_proto_endpoints_info_t *data);
+extern int weka_proto_set_endpoints_info(int slot, weka_proto_endpoints_info_t *data);
+extern int weka_proto_set_docache(int slot, int val);
+extern int weka_proto_inc_subxid(int);
+extern int weka_proto_clear_endpoints_info(int slot);
+#endif
+
+#endif /* __WEKA_PROTO_H__ */
--- src/work_pool.c
+++ src/work_pool.c
@@ -59,12 +59,15 @@
 
 #include <rpc/work_pool.h>
 
+#include "misc/weka_proto.h"
+
 #define WORK_POOL_STACK_SIZE MAX(1 * 1024 * 1024, PTHREAD_STACK_MIN)
 #define WORK_POOL_TIMEOUT_MS (31 /* seconds (prime) */ * 1000)
 
 /* forward declaration in lieu of moving code, was inline */
 
 static int work_pool_spawn(struct work_pool *pool);
+__thread int weka_proto_slot;
 
 int
 work_pool_init(struct work_pool *pool, const char *name,
@@ -128,6 +131,16 @@ work_pool_init(struct work_pool *pool, const char *name,
 
 	/* initial spawn will spawn more threads as needed */
 	pool->n_threads = 1;
+
+	/* Initialize weka protocol driver */
+	rc = weka_proto_init();
+	if (rc) {
+		__warnx(TIRPC_DEBUG_FLAG_ERROR,
+			"%s() unable to initialize weka proto driver : %s (%d)",
+			__func__, strerror(rc), rc);
+		return rc;
+	}
+
 	return work_pool_spawn(pool);
 }
 
@@ -156,10 +169,16 @@ work_pool_thread(void *arg)
 	pthread_mutex_lock(&pool->pqh.qmutex);
 	TAILQ_INSERT_TAIL(&pool->wptqh, wpt, wptq);
 
+	weka_proto_slot = weka_proto_alloc_slot();
+
 	wpt->worker_index = atomic_inc_uint32_t(&pool->worker_index);
 	snprintf(wpt->worker_name, sizeof(wpt->worker_name), "%.5s%" PRIu32,
 		 pool->name, wpt->worker_index);
 	__ntirpc_pkg_params.thread_name_(wpt->worker_name);
+
+	__warnx(TIRPC_DEBUG_FLAG_WORKER,
+			"%s() %s",
+			__func__, wpt->worker_name);
 
 	do {
 		/* testing at top of loop allows pre-specification of work,
@@ -236,6 +255,8 @@ work_pool_thread(void *arg)
 	TAILQ_REMOVE(&pool->wptqh, wpt, wptq);
 	pthread_mutex_unlock(&pool->pqh.qmutex);
 
+	weka_proto_free_slot(weka_proto_slot);
+
 	__warnx(TIRPC_DEBUG_FLAG_WORKER,
 		"%s() %s terminating",
 		__func__, wpt->worker_name);
@@ -307,6 +328,8 @@ work_pool_shutdown(struct work_pool *pool)
 		.tv_nsec = 3000,
 	};
 
+	int rc;
+
 	pthread_mutex_lock(&pool->pqh.qmutex);
 	pool->timeout_ms = 1;
 	pool->params.thrd_max =
@@ -330,6 +353,14 @@ work_pool_shutdown(struct work_pool *pool)
 
 	mem_free(pool->name, 0);
 	poolq_head_destroy(&pool->pqh);
+
+	/* release per thread slots */
+	rc = weka_proto_shutdown();
+	if (rc) {
+		__warnx(TIRPC_DEBUG_FLAG_ERROR,
+			"%s() failed to shutdown weka proto driver : %s (%d)",
+			__func__, strerror(rc), rc);
+	}
 
 	return (0);
 }
